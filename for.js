2669a6618968cbcfb89855738fef04238e8ec004
Fagon9831 <fgonzalezes@hotmail.com>
2023-06-19 18:37:46 -0500

    Segundo  Commit

---
+++ for.js
@@ -0,0 +1,22 @@
+//Ejercicio#1
+/*for (let index = 1; index <=50; index++) {    
+    if (index%2==1) {
+        console.log(index)    
+    }    
+}*/
+//Ejercicio#2
+/*
+var pokemons = [ 'bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'charizard', 'squirtle', 'wartortle', 'blastoise', 'caterpie', 'metapod', 'butterfree', 'weedle', 'kakuna', 'beedrill', 'pidgey', 'pidgeotto', 'pidgeot', 'rattata', 'raticate', 'spearow', 'fearow', 'ekans', 'arbok', 'pikachu', 'raichu', 'sandshrew', 'sandslash', 'nidoran-f', 'nidorina', 'nidoqueen', 'nidoran-m', 'nidorino', 'nidoking', 'clefairy', 'clefable', 'vulpix', 'ninetales', 'jigglypuff', 'wigglytuff', 'zubat', 'golbat', 'oddish', 'gloom', 'vileplume', 'paras', 'parasect', 'venonat', 'venomoth', 'diglett', 'dugtrio', 'meowth', 'persian', 'psyduck', 'golduck', 'mankey', 'primeape', 'growlithe', 'arcanine', 'poliwag', 'poliwhirl', 'poliwrath', 'abra', 'kadabra', 'alakazam', 'machop', 'machoke', 'machamp', 'bellsprout', 'weepinbell', 'victreebel', 'tentacool', 'tentacruel', 'geodude', 'graveler', 'golem', 'ponyta', 'rapidash', 'slowpoke', 'slowbro', 'magnemite', 'magneton', 'farfetchd', 'doduo', 'dodrio', 'seel', 'dewgong', 'grimer', 'muk', 'shellder', 'cloyster', 'gastly', 'haunter', 'gengar', 'onix', 'drowzee', 'hypno', 'krabby', 'kingler', 'voltorb']
+const NumLimit= window.prompt("Ingrese un numero")
+for (var i = 0; i <= NumLimit; i++) {        
+    if (i%5==0) {
+       console.log(pokemons[i])
+    }
+}*/
+//Ejercicio#3
+var sTXT= [4,"dos",8,"tres",5,9,1,"cero"]
+for (let i = 0; i < sTXT.length; i++) {
+    if (typeof sTXT[i] === "number") {
+        console.log(sTXT[i])
+    }    
+}
